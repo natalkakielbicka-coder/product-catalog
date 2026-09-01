25689fe7e52d1121119c76a6cdccdbdfc7cbe26c
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-01 14:14:12 +0200

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/product-catalog/',
+
+  plugins: [vue(), vueDevTools()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
